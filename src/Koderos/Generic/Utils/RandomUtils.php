ba7d7dc828f1f2213edf8354ce8b6303a2a95d9c
cepa <lukasz@cepowski.com>
2022-12-20 17:08:09 +0100

    Fix RandomUtils

--- src/Koderos/Generic/Utils/RandomUtils.php
+++ src/Koderos/Generic/Utils/RandomUtils.php
@@ -6,10 +6,10 @@ class RandomUtils
 {
     const DEFAULT_DICT = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    public static function randomString(int $n, ?array $dict = null): string
+    public static function randomString(int $n, $dict = null): string
     {
-        $dict = is_array($dict) ? $dict : self::DEFAULT_DICT;
-        $m = strlen($dict);
+        $dict = isset($dict) ? $dict : self::DEFAULT_DICT;
+        $m = is_string($dict) ? strlen($dict) : count($dict);
         $s = '';
         $n++;
         while (--$n) {
--- test/Tests/Koderos/Generic/RandomUtilsTest.php
+++ test/Tests/Koderos/Generic/RandomUtilsTest.php
@@ -12,4 +12,16 @@ class RandomUtilsTest extends TestCase
       $x = RandomUtils::randomString(16);
       $this->assertEquals(16, strlen($x));
   }
+
+  public function testRandomStringWithStringDict()
+  {
+    $x = RandomUtils::randomString(16, 'abcd');
+    $this->assertEquals(16, strlen($x));
+  }
+
+  public function testRandomStringWithArrayDict()
+  {
+    $x = RandomUtils::randomString(9, ['abc', 'def', 'ghi']);
+    $this->assertEquals(9 * 3, strlen($x));
+  }
 }
